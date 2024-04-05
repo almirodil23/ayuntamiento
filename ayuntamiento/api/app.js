e8b5af3f518d8e80603b888309045653a1d448e5
Alvaro <almirodil@gmail.com>
2024-04-05 12:04:03 +0200

    actualizacion Heroku 68

--- ayuntamiento/api/app.js
+++ ayuntamiento/api/app.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const app = express(); 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 var cors = require('cors');
 const enrutamientoProovedores = require('./rutas.js')
